be160204d781a0e5335eb2e6a6ec99fb7858914f
vmr <vmrbaez@gmail.com>
2023-06-30 19:16:31 -0600

    Se creo el modelo de server

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
+const Server = require('./models/server');
+
 require('dotenv').config();
-const express = require('express');
-const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello word')
-});
-app.get('*', (req, res) => {
-    res.send('404')
-});
+const server = new Server();
+
+server.listen()
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor corriendo en: ${process.env.PORT}`)
-});
+
+
+
+
